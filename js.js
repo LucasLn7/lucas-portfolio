644cb98dd3019c4fa32a9d2edc2d06c12a04bad9
Lucas lima <lucasliman4@gmail.com>
2026-04-19 21:02:59 -0300

    melhoramos o arquivo stylers

---
+++ js.js
@@ -0,0 +1,5 @@
+const none = document.getElementById('none');
+const mensagem = document.getElementById('mensagem');
+
+const telefone = '5511999999999'
+const texto = `Olá, meu nome é ${none.value} e minha mensagem é: ${mensagem.value}`;
